8b77d4ca67b080da339d18d77fddcbb002ee8867
arthurpassere <passerearthur@gmail.com>
2025-01-05 21:34:20 -0300

    refactor(forms): separate functions from onsubmit event
    - Removed inline functions from form.onsubmit
    - Created standalone functions and called them in the event handler
    - Improved code readability and organization

--- script.js
+++ script.js
@@ -26,76 +26,77 @@ form.onsubmit = (e) =>{
     category_name: category.options[category.selectedIndex].textContent, 
   }
 
-  function moneyFormat(money){
-    return new Intl.NumberFormat("pt-BR", {
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2
-    }).format(money);
-  }
-
-  function addExpense(solicitation){
-
-    const newLi = document.createElement("li");
-    newLi.className = "expense";
-    
-    const typeIcon = document.createElement("img");
-    typeIcon.setAttribute("src", `./img/${solicitation.category_id}.svg`);
-    typeIcon.setAttribute("alt", "Ícone de tipo da despesa");
-    newLi.appendChild(typeIcon);
-
-    const newDiv = document.createElement("div");
-    newDiv.className = "expense-info";
-
-    const expName = document.createElement("strong");
-    expName.textContent = solicitation.name;
-
-    const catName = document.createElement("span");
-    catName.textContent = solicitation.category_name;
-
-    newDiv.appendChild(expName);
-    newDiv.appendChild(catName);
-    newLi.appendChild(newDiv);
-
-    const expAmount = document.createElement("span")
-    expAmount.className = "expense-amount"
-
-    const amountSimble = document.createElement("small")
-    amountSimble.textContent = "R$"
-    expAmount.appendChild(amountSimble)
-    expAmount.textContent += (" " + moneyFormat(parseFloat(solicitation.amount.replace(",","."))))
-
-    newLi.appendChild(expAmount)
-
-    const removeIcon = document.createElement("img")
-    removeIcon.setAttribute("src", "./img/remove.svg")
-    removeIcon.setAttribute("alt", "remover")
-    removeIcon.className = "remove-icon"
-
-    removeIcon.addEventListener("click", (e) => {
-      lists.removeChild(newLi);
-      numSolicitations -= 1;
-      totalSolicitations -= parseFloat(solicitation.amount.replace(',', '.'));
-      totalExpenses.textContent = (numSolicitations + " despesas");
-      totalAmount.innerHTML = `<small>R$</small> ${moneyFormat(totalSolicitations)}`;
-      if (numSolicitations == 0 && totalSolicitations != 0){
-        totalSolicitations = 0
-        totalAmount.innerHTML = `<small>R$</small> ${moneyFormat(totalSolicitations)}`
-    }});
-  
-
-    newLi.appendChild(removeIcon)
-    lists.appendChild(newLi)
-  }
-  
-  function updateTotals(solicitation){
-    numSolicitations += 1
-    totalExpenses.textContent = (numSolicitations + " despesas") // atualiza numero de despesas
-    
-    totalSolicitations += parseFloat(solicitation.amount.replace(',', '.'));
-    totalAmount.innerHTML = `<small>R$</small> ${moneyFormat(totalSolicitations)}`;
-
-  }
   updateTotals(solicitation)
   addExpense(solicitation)
+}
+
+function moneyFormat(money){
+  return new Intl.NumberFormat("pt-BR", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }).format(money);
+}
+
+function addExpense(solicitation){
+
+  const newLi = document.createElement("li");
+  newLi.className = "expense";
+  
+  const typeIcon = document.createElement("img");
+  typeIcon.setAttribute("src", `./img/${solicitation.category_id}.svg`);
+  typeIcon.setAttribute("alt", "Ícone de tipo da despesa");
+  newLi.appendChild(typeIcon);
+
+  const newDiv = document.createElement("div");
+  newDiv.className = "expense-info";
+
+  const expName = document.createElement("strong");
+  expName.textContent = solicitation.name;
+
+  const catName = document.createElement("span");
+  catName.textContent = solicitation.category_name;
+
+  newDiv.appendChild(expName);
+  newDiv.appendChild(catName);
+  newLi.appendChild(newDiv);
+
+  const expAmount = document.createElement("span")
+  expAmount.className = "expense-amount"
+
+  const amountSimble = document.createElement("small")
+  amountSimble.textContent = "R$"
+  expAmount.appendChild(amountSimble)
+  expAmount.textContent += (" " + moneyFormat(parseFloat(solicitation.amount.replace(",","."))))
+
+  newLi.appendChild(expAmount)
+
+  const removeIcon = document.createElement("img")
+  removeIcon.setAttribute("src", "./img/remove.svg")
+  removeIcon.setAttribute("alt", "remover")
+  removeIcon.className = "remove-icon"
+
+  removeIcon.addEventListener("click", (e) => {
+    lists.removeChild(newLi);
+    numSolicitations -= 1;
+    totalSolicitations -= parseFloat(solicitation.amount.replace(',', '.'));
+    totalExpenses.textContent = (numSolicitations + " despesas");
+    totalAmount.innerHTML = `<small>R$</small> ${moneyFormat(totalSolicitations)}`;
+    if (numSolicitations == 0 && totalSolicitations != 0){
+      totalSolicitations = 0
+      totalAmount.innerHTML = `<small>R$</small> ${moneyFormat(totalSolicitations)}`
+  }});
+
+
+  newLi.appendChild(removeIcon)
+  lists.appendChild(newLi)
+}
+
+function updateTotals(solicitation){
+  numSolicitations += 1
+  totalExpenses.textContent = (numSolicitations + " despesas") // atualiza numero de despesas
+  
+  totalSolicitations += parseFloat(solicitation.amount.replace(',', '.'));
+  totalAmount.innerHTML = `<small>R$</small> ${moneyFormat(totalSolicitations)}`;
+
 }
 
